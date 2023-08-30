53985e4d8a2f2cc22547cc048f9a3a7f34613cc3
issaevis <evis.topollaj@gmail.com>
2023-08-30 12:41:54 +0200

    Fixed 7

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
-from typing import Union
+from typing import Union, Tuple
 '''type annotated function'''
 
 
-def to_kv(k: str, v: Union[int, float]) -> tuple:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     '''func that returns a tuple'''
     ret = (k, v**2)
 
