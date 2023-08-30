ee4e8eaa20fce9b529a84ef30ebe93b4d02c0871
issaevis <evis.topollaj@gmail.com>
2023-08-30 12:28:27 +0200

    Added a small fix to 6

--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -3,7 +3,7 @@ from typing import List, Union
 '''type annotated function'''
 
 
-def sum_mixed_list(mxd_lst: List[Union[float, int]]) -> float:
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     sum = 0
     for number in mxd_lst:
         sum += number
