485b375f1a9c521abc239ebb7555e967ec522655
issaevis <evis.topollaj@gmail.com>
2023-08-31 18:46:00 +0200

    Added list comprehension in 1

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -11,8 +11,4 @@ async_generator = __import__('0-async_generator').async_generator
 async def async_comprehension() -> typing.List[float]:
     '''returns a list of floats to the main'''
 
-    result = []
-    async for i in async_generator():
-        result.append(i)
-
-    return result
+    return [i async for i in async_generator()]
