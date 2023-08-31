3620f509e0a7a2e18af1f324617ebd22f91c7460
issaevis <evis.topollaj@gmail.com>
2023-08-31 18:34:28 +0200

    Fixed 0

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -3,10 +3,12 @@
 
 import asyncio
 from random import triangular
+import typing
 
-async def async_generator():
+
+async def async_generator() -> typing.Generator[float, NoneType, NoneType]:
     '''yields a random float'''
 
     for i in range(10):
         await asyncio.sleep(1)
-        yield(triangular(0, 10))
+        yield triangular(0, 10)
