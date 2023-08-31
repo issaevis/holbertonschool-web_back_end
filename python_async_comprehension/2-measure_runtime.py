9decf9a5dc061fbdf74773140291681b4b409c85
issaevis <evis.topollaj@gmail.com>
2023-08-31 19:03:47 +0200

    Ficced 3

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -9,6 +9,8 @@ async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
 async def measure_runtime() -> float:
+    '''returns the elapsed time for async_comprehension
+    to run 4 times in parallel'''
     s_time = time.time()
     tasks = [async_comprehension() for i in range(4)]
     await asyncio.gather(*tasks)
