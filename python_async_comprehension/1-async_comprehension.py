cbe98a3f0c6114db58949502559998fdfed7f5cd
issaevis <evis.topollaj@gmail.com>
2023-08-31 18:42:21 +0200

    Ficced a lil ting in 1

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -8,7 +8,9 @@ import typing
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() -> typing.List:
+async def async_comprehension() -> typing.List[float]:
+    '''returns a list of floats to the main'''
+
     result = []
     async for i in async_generator():
         result.append(i)
