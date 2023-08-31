b28e08e5472b86edc9b261afe5c55835a17162bd
issaevis <evis.topollaj@gmail.com>
2023-08-31 15:33:22 +0200

    Added a fix to 2

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -1,24 +1,16 @@
 #!/usr/bin/env python3
-'''async function module'''
-import asyncio
-from random import triangular
+''' async func module'''
 import time
+import asyncio
 
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
 def measure_time(n: int, max_delay: int) -> float:
-    total_time = 0
+    '''messure the time of wait_n'''
+    start_time = time.time()
 
-    loop = asyncio.get_event_loop()
-
-    for _ in range(n):
-        start_time = time.time()
-
-        result = loop.run_until_complete(wait_n(n, max_delay))
-
-        end_time = time.time()
-        total_time += (end_time - start_time)
-
-    average_time = total_time / n
-    return average_time
+    asyncio.run(wait_n(n, max_delay))
+    end_time = time.time()
+    avg_time = end_time - start_time
+    return avg_time / n
