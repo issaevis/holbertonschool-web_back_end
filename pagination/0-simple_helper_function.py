2ea53e4730e4ca4d9c8b294146cced14568b7001
issaevis <evis.topollaj@gmail.com>
2023-09-06 16:06:58 +0200

    Added 0 and README

---
+++ pagination/0-simple_helper_function.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+'''simple helper function'''
+
+
+def index_range(page: int, page_size: int) -> tuple:
+    '''function that returns a tuple'''
+
+    start_index = (page - 1) * page_size
+    end_index = start_index + page_size
+
+    return (start_index, end_index)
