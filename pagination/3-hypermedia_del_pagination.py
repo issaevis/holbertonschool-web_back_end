5b229a0916c9af9ad5e9e9f1220795de2bad6ec7
issaevis <evis.topollaj@gmail.com>
2023-09-06 17:18:12 +0200

    Added 3

--- pagination/3-hypermedia_del_pagination.py
+++ pagination/3-hypermedia_del_pagination.py
@@ -6,7 +6,7 @@ from typing import List, Dict
 
 
 class Server:
-        """Server class to paginate a database of popular baby names.
+    """Server class to paginate a database of popular baby names.
     """
     DATA_FILE = "Popular_Baby_Names.csv"
 
