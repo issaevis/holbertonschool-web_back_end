09feb6356570a36ea11d79efa534ff91975ee7cb
issaevis <evis.topollaj@gmail.com>
2023-09-10 18:26:28 +0200

    Added 101

---
+++ NoSQL/101-students.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+'''function that returns all students sorted by average score'''
+
+
+import pymongo
+
+
+def top_students(mongo_collection):
+    pipeline = [
+        {"$unwind": "$topics"},
+        {
+            "$group": {
+                "_id": "$name",
+                "averageScore": {"$avg": "$topics.score"},
+                "originalDoc": {"$first": "$$ROOT"}
+            }
+        },
+        {"$sort": {"averageScore": -1}}
+    ]
+
+    results = mongo_collection.aggregate(pipeline)
+
+    # Format the results to include the _id
+    formatted_results = []
+    for result in results:
+        formatted_results.append({
+            "_id": result["originalDoc"]["_id"],
+            "name": result["_id"],
+            "averageScore": result["averageScore"]
+        })
+
+    return formatted_results
