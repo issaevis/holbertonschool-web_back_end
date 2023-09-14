a974647032f86dcef166387ead40fa708dbc29ea
issaevis <evis.topollaj@gmail.com>
2023-09-14 16:53:24 +0200

    Added a fix to 1

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -1,10 +1,14 @@
 export default function taskBlock(trueOrFalse) {
+  /* eslint-disable no-unused-vars */
   const task = false;
   const task2 = true;
+  /* eslint-enable no-unused-vars */
 
   if (trueOrFalse) {
+    /* eslint-disable no-unused-vars */
     const task = true;
     const task2 = false;
+    /* eslint-enable no-unused-vars */
   }
 
   return [task, task2];
