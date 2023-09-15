e75c83739e2ece8e13e0bbc8d73e4c341498c04e
issaevis <evis.topollaj@gmail.com>
2023-09-15 19:15:28 +0200

    Added 6

---
+++ ES6_classes/6-sky_high.js
@@ -0,0 +1,22 @@
+/* eslint-disable no-underscore-dangle */
+// eslint-disable-next-line import/extensions
+import Building from './5-building.js';
+
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super(sqft);
+    this.floors = floors;
+  }
+
+  get floors() {
+    return this._floors;
+  }
+
+  set floors(floors) {
+    this._floors = floors;
+  }
+
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this.floors} floors`;
+  }
+}
