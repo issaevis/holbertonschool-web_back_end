c4a02d1ed2376ebea385431a0e886f6f9679b5f5
issaevis <evis.topollaj@gmail.com>
2023-09-15 15:46:20 +0200

    Added 3

---
+++ ES6_classes/3-currency.js
@@ -0,0 +1,29 @@
+/* eslint-disable no-underscore-dangle */
+export default class Currency {
+  constructor(code, name) {
+    this.code = code;
+    this.name = name;
+  }
+  // Getters and setters for the attributes
+
+  get code() {
+    return this._code;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set code(code) {
+    this._code = code;
+  }
+
+  set name(name) {
+    this._name = name;
+  }
+
+  // Method and all that
+  displayFullCurrency() {
+    return `${this.name}(${this.code})`;
+  }
+}
