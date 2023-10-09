3fa1b24c93b2638c3ef732be73f6e0db35821fef
issaevis <evis.topollaj@gmail.com>
2023-10-09 16:57:42 +0200

    Added 3

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -1,14 +1,16 @@
-import {uploadPhoto, createUser} from "utils";
+import { uploadPhoto, createUser } from './utils';
 
 function handleProfileSignup() {
-    const photoPromise = uploadPhoto();
-    const userPromise = createUser();
+  const photoPromise = uploadPhoto();
+  const userPromise = createUser();
 
-    Promise.all([photoPromise, userPromise])
-        .then(([photo, user]) => {
-            console.log(`photo-${photo} ${user.firstName} ${user.lastName}`);
-        })
-        .catch(() => {
-            console.log('Signup system offline');
-        });
+  Promise.all([photoPromise, userPromise])
+    .then(([photo, user]) => {
+      console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
+    })
+    .catch(() => {
+      console.log('Signup system offline');
+    });
 }
+
+export default handleProfileSignup;
